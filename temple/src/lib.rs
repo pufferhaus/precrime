7b98eee3b6eb5f7a49562026e9cf3d453c289940
iMi <pufferhaus@fugu.estate>
2026-05-17 11:25:51 -0700

    temple: bootstrap workspace crate

---
+++ temple/src/lib.rs
@@ -0,0 +1,6 @@
+//! PRECRIME temple ball.
+
+pub const DEFAULT_TEMPLE_GROUP: &str = "239.42.0.1";
+pub const DEFAULT_TEMPLE_PORT: u16 = 9999;
+pub const BALL_PERIOD_SECS: u64 = 2;
+pub const BALL_EVICTION_SECS: u64 = 6;
