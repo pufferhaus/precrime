97a7e3f6b6da08402b75062fe7014660b924a220
imi <pufferhaus@fugu.estate>
2026-05-18 23:04:37 -0700

    fix: video orientation portrait

--- ios/Witness/Witness/Capture/CaptureSession.swift
+++ ios/Witness/Witness/Capture/CaptureSession.swift
@@ -117,7 +117,7 @@ final class CaptureSession: NSObject, AVCaptureVideoDataOutputSampleBufferDelega
 
         if let connection = videoOutput.connection(with: .video) {
             if connection.isVideoOrientationSupported {
-                connection.videoOrientation = .landscapeLeft
+                connection.videoOrientation = .portrait
             }
         }
     }
